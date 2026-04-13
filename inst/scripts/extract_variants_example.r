671bd4c8fc579a387b7d0bd434da4abc646d6f1b
cw1245 <cw1245@exeter.ac.uk>
2026-04-13 15:50:43 +0100

    Added old version to cross-check

--- inst/scripts/extract_variants_example.r
+++ inst/scripts/extract_variants_example.r
@@ -1,9 +1,16 @@
 library(dplyr)
 library(readr)
 
+remotes::install_github("icallumwebb/ukbrapR_Callum", force = TRUE)
+varlist <- data.frame(rsid=c("rs72725854"), chr=c(8))
+imputed_genotypes <- ukbrapR:::extract_variants(varlist, overwrite=TRUE, 
+                                                progress=TRUE, verbose=TRUE, very_verbose=TRUE)
+
 ## Old version - successfully extracts rs72725854_G AND rs72725854_T
 
-remotes::install_github("icallumwebb/ukbrapR_Callum", force = TRUE)
+.rs.restartR()
+remove.packages("ukbrapR")
+remotes::install_github("lcpilling/ukbrapR@v0.3.9", force = TRUE)
 varlist <- data.frame(rsid=c("rs72725854"), chr=c(8))
 imputed_genotypes <- ukbrapR:::extract_variants(varlist, overwrite=TRUE, 
                                                 progress=TRUE, verbose=TRUE, very_verbose=TRUE)
